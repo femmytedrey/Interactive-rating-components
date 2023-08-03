b70e4b64efb6f83008f99a9d2cbf24d2b9f9ae7e
femmytedrey <femmytedrey@gmail.com>
2023-08-03 20:05:37 +0100

    progress

--- script.js
+++ script.js
@@ -4,9 +4,18 @@ const circleCheckboxes = document.querySelectorAll('input[name="circle"]');
 
 // Add a click event listener to the submit button
 submitButton.addEventListener("click", () => {
-    // Count the number of selected circles
-    const numSelectedCircles = [...circleCheckboxes].filter(checkbox => checkbox.checked).length;
+  // Initialize a variable to count the number of selected circles
+  let numSelectedCircles = 0;
 
-    // Navigate to the "submitted.html" page with the selected number as a query parameter
-    window.location.href = `submitted.html?selected=${numSelectedCircles}`;
+  // Loop through the circle checkboxes to check which ones are selected
+  circleCheckboxes.forEach(checkbox => {
+    if (checkbox.checked) {
+      numSelectedCircles++;
+    }
+  });
+
+  console.log('Number of selected circles:', numSelectedCircles);
+
+  // Navigate to the "submitted.html" page with the selected number as a query parameter
+  window.location.href = `submitted.html?selected=${numSelectedCircles}`;
 });
--- submitted.js
+++ submitted.js
@@ -2,6 +2,6 @@
 const urlParams = new URLSearchParams(window.location.search);
 const numSelectedCircles = urlParams.get('selected');
 
-// Display the message in the "selectedmessage" element
+// Display the message in the "submittedmessage" element
 const selectedMessageElement = document.querySelector('.submittedmessage');
 selectedMessageElement.textContent = `You selected ${numSelectedCircles} out of 5`;

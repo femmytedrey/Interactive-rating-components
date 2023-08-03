a8aa856945ba32a0e61f5da3b98518b39435159a
femmytedrey <femmytedrey@gmail.com>
2023-08-03 22:49:43 +0100

    progress

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
-// Get all the checkbox elements with name="circle"
 const checkboxes = document.querySelectorAll('input[name="circle"]');
 
-// Function to count the selected checkboxes
 const countSelectedCheckboxes = () => {
-  // Initialize a variable to keep track of the number of selected checkboxes
-  let selectedCount = 0;
 
-  // Loop through all checkboxes and count the selected ones
-  checkboxes.forEach(checkbox => {
+    let selectedCount = 0;
+
+
+    checkboxes.forEach(checkbox => {
     if (checkbox.checked) {
       selectedCount++;
     }
@@ -16,17 +14,17 @@ const countSelectedCheckboxes = () => {
   return selectedCount;
 };
 
-// Function to handle the "SUBMIT" button click
-const handleFormSubmit = () => {
-  // Get the number of selected checkboxes
-  const selectedCount = countSelectedCheckboxes();
 
-  // Redirect to submit.html with the selectedCount as a URL parameter
-  window.location.href = `submit.html?selectedCount=${selectedCount}`;
+const handleFormSubmit = () => {
+
+    const selectedCount = countSelectedCheckboxes();
+
+
+    window.location.href = `submit.html?selectedCount=${selectedCount}`;
 };
 
-// Get the "SUBMIT" button element
+
 const submitButton = document.getElementById('submitButton');
 
-// Add an event listener to the "SUBMIT" button to detect clicks
+
 submitButton.addEventListener('click', handleFormSubmit);
--- submitted.js
+++ submitted.js
@@ -1,7 +1,5 @@
-// Get the selectedCount from the URL parameter
 const urlParams = new URLSearchParams(window.location.search);
 const selectedCount = urlParams.get('selectedCount');
 
-// Update the message with the correct selected count
 const selectedMessage = document.getElementById('selectedMessage');
 selectedMessage.textContent = `You selected ${selectedCount} out of 5`;
